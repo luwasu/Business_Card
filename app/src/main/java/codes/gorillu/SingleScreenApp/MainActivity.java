1c74f049511256f539d514daaf81ac9f2b1e4cc6
LuSantanaHolmes <luis.santanaholmes@gmail.com>
2020-01-25 22:57:24 +0100

    run google maps with a marker on Bern with a title

--- app/src/main/java/codes/gorillu/SingleScreenApp/MainActivity.java
+++ app/src/main/java/codes/gorillu/SingleScreenApp/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        // Add a marker in Bern and move the camera
+        LatLng bern = new LatLng(46.944680, 7.423410);
+        mMap.addMarker(new MarkerOptions().position(bern).title("Gorillu Codes"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bern, 15));
+
+
     }
 }
